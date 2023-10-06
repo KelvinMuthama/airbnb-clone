b86e9b090578aa3a5f5f94e08f8d30400f39ad69
kelvinmuthama <kelvinkamau387@gmail.com>
2023-10-06 15:14:14 +0300

    feat: Add filters and search functionality

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -2,11 +2,27 @@ import prisma from "@/app/libs/prismadb";
 
 export interface IListingParams {
   userId?: string;
+  guestCount?: number;
+  roomCount?: number;
+  bathroomCount?: number;
+  startDate?: string;
+  endDate?: string;
+  locationValue?: string;
+  category?: string;
 }
 
 export default async function getListings(params: IListingParams) {
   try {
-    const { userId } = params;
+    const {
+      userId,
+      roomCount,
+      guestCount,
+      bathroomCount,
+      locationValue,
+      startDate,
+      endDate,
+      category,
+    } = params;
 
     let query: any = {};
 
@@ -14,7 +30,51 @@ export default async function getListings(params: IListingParams) {
       query.userId = userId;
     }
 
+    if (category) {
+      query.category = category;
+    }
+
+    if (roomCount) {
+      query.roomCount = {
+        gte: +roomCount,
+      };
+    }
+    if (guestCount) {
+      query.guestCount = {
+        gte: +guestCount,
+      };
+    }
+    if (bathroomCount) {
+      query.bathroomCount = {
+        gte: +bathroomCount,
+      };
+    }
+
+    if (locationValue) {
+      query.locationValue = locationValue;
+    }
+
+    if (startDate && endDate) {
+      query.NOT = {
+        reservations: {
+          some: {
+            OR: [
+              {
+                endDate: { gte: startDate },
+                startDate: { lte: startDate },
+              },
+              {
+                startDate: { lte: endDate },
+                endDate: { gte: endDate },
+              },
+            ],
+          },
+        },
+      };
+    }
+
     const listings = await prisma.listing.findMany({
+      where: query,
       orderBy: {
         createdAt: "desc",
       },
@@ -26,8 +86,7 @@ export default async function getListings(params: IListingParams) {
     }));
 
     return safeListings;
-  } catch (error) {
-    // Simply rethrow the original error
-    throw error;
+  } catch (error: any) {
+    throw new Error(error);
   }
 }
